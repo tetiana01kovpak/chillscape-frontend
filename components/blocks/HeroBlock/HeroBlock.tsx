49de8d9388d2fd822a13d0587ab089cbcb5d606f
Demchenko Oksana <sulichka33@gmail.com>
2026-03-29 19:59:18 +0300

    WIP: Hero changes before pull

--- components/blocks/HeroBlock/HeroBlock.tsx
+++ components/blocks/HeroBlock/HeroBlock.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import styles from './HeroBlock.module.css';
+import { Input } from '@/components/ui/Input/Input';
+import { Button } from '@/components/ui/Button/Button';
 
 const HeroBlock = () => {
   const [search, setSearch] = useState('');
@@ -24,7 +26,7 @@ const HeroBlock = () => {
             alt="Hero Background"
             fill
             style={{ objectFit: 'cover' }}
-            sizes='(max-width: 375px) 100vw, (max-width: 1440px) 1400px, 100vw'
+            sizes="(max-width: 375px) 100vw, (max-width: 1440px) 1400px, 100vw"
             priority
           />
         </div>
@@ -38,16 +40,19 @@ const HeroBlock = () => {
           </p>
 
           <div className={styles.search}>
-            <input
-              type="text"
+            <Input
               placeholder="Введіть назву, тип або регіон..."
               value={search}
               onChange={(e) => setSearch(e.target.value)}
-              className={styles.input}
+              className={styles.heroInput}
             />
-            <button onClick={handleSearch} className={styles.button}>
+            <Button
+              onClick={handleSearch}
+              variant="primary"
+              className={styles.heroButton}
+            >
               Знайти місце
-            </button>
+            </Button>
           </div>
         </div>
       </section>
