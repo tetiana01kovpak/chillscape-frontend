bcb41005e12d5fbd5e38db256856c1af3ceec7c5
Tetiana <tetianakovpak@gmail.com>
2026-03-30 15:50:10 +0300

    Add copyright

--- components/layout/AuthFooter/AuthFooter.tsx
+++ components/layout/AuthFooter/AuthFooter.tsx
@@ -3,7 +3,7 @@ import css from './AuthFooter.module.css';
 export default function AuthFooter() {
   return (
     <footer className={css.footer}>
-      <p className={css.text}>© 2025 Relax Map</p>
+      <p className={css.text}>© 2026 Relax Map</p>
     </footer>
   );
 }
