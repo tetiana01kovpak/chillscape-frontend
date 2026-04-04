79d9855bf911e6b7dcecd90b036cc12cb01468a7
Demchenko Oksana <sulichka33@gmail.com>
2026-04-04 14:39:57 +0300

    fix: pass location id to AddReviewModal

--- app/(main)/add-review/page.tsx
+++ app/(main)/add-review/page.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import AddReviewModal from '@/components/blocks/AddReviewModal/AddReviewModal';
-import { useRouter } from 'next/navigation';
+import { useRouter, useParams } from 'next/navigation';
 
 export default function AddReviewPage() {
   const router = useRouter();
+  const params = useParams(); 
+  const locationId = params.locationId as string; 
 
   const handleClose = () => {
     router.back();
   };
 
-  return <AddReviewModal onClose={handleClose} />;
+  return <AddReviewModal onClose={handleClose} locationId={locationId} />;
 }
